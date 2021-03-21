36aa3fa85b57794836b4cf9b08e1e7736c504aa8
tianiue <61725402+tianiue@users.noreply.github.com>
2021-03-21 17:43:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -31,3 +31,5 @@ git clone https://github.com/garypang13/luci-app-dnsfilter.git package/luci-app-
 git clone https://github.com/tianiue/luci-app-bypass.git package/luci-app-bypass
 git clone https://github.com/esirplayground/luci-app-LingTiGameAcc.git package/luci-app-LingTiGameAcc
 git clone https://github.com/esirplayground/LingTiGameAcc.git package/LingTiGameAcc
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
